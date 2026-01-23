b6b6a7d8184637631c259d830c22b92bb62ee821
AlbertWebs <albertmuhatia@gmail.com>
2026-01-23 18:34:21 +0300

    update

--- resources/views/front/menu.blade.php
+++ resources/views/front/menu.blade.php
@@ -1,20 +1,15 @@
 <ul class="navigation clearfix">
-    {{-- <li class="current"><a href="{{url('/')}}"><span class="fa fa-home"></span> Home</a>
+    <li class="current"><a href="{{url('/')}}"><span class="fa fa-home"></span> Home</a>
 
-    </li> --}}
-    <li><a href="{{url('/')}}/the-company"><span class="fa fa-info"></span> The Company</a>
+    </li> 
+    <li><a href="{{url('/')}}/laptops-for-hire"><span class="fa fa-laptop"></span> Hire </a>
+        {{-- <ul>
+
+            <li><a download href="{{url('/')}}/uploads/Laptops-Hire-Brochure.pdf">Download Profile <span class="fa fa-download"></span></a></li>
+
+        </ul> --}}
 
     </li>
-
-    <li class="dropdown"><a href="#">Center of Exellence</a>
-        <ul>
-            <?php $Services = DB::table('services')->get(); ?>
-            @foreach ($Services as $Ser)
-            <li><a href="{{url('/')}}/center-of-excellence/{{$Ser->slung}}">{{$Ser->title}}</a></li>
-            @endforeach
-        </ul>
-    </li>
-
     <li><a href="{{url('/')}}/e-commerce"><span class="fa fa-shopping-cart"></span>&nbsp; Shop Online</a>
         {{-- <ul>
             <li><a href="{{url('/')}}/e-commerce/product/tags/laptops">Laptops</a></li>
@@ -25,12 +20,19 @@
 
         </ul> --}}
     </li>
-    <li><a href="{{url('/')}}/laptops-for-hire"><span class="fa fa-laptop"></span> Hire </a>
-        {{-- <ul>
-
-            <li><a download href="{{url('/')}}/uploads/Laptops-Hire-Brochure.pdf">Download Profile <span class="fa fa-download"></span></a></li>
-
-        </ul> --}}
+   
+    <li><a href="{{url('/')}}/the-company"><span class="fa fa-info"></span> The Company</a>
 
     </li>
+
+    <li class="dropdown"><a href="#">Our Services</a>
+        <ul>
+            <?php $Services = DB::table('services')->get(); ?>
+            @foreach ($Services as $Ser)
+            <li><a href="{{url('/')}}/center-of-excellence/{{$Ser->slung}}">{{$Ser->title}}</a></li>
+            @endforeach
+        </ul>
+    </li>
+
+   
 </ul>
